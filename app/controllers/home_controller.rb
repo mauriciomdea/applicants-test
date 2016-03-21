8e743ecb31ec64a60a4473d8ff1aa077d022beaf
Mauricio Almeida <mauriciomdea@gmail.com>
2016-03-21 03:15:04 +0000

    Refactored finding and creating new makers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,20 +2,17 @@ class HomeController < ApplicationController
   
   def index
 
-    #search the make
+    # Search the maker
     uri = URI("http://www.webmotors.com.br/carro/marcas")
 
     # Make request for Webmotors site
     response = Net::HTTP.post_form(uri, {})
     json = JSON.parse response.body
 
-    # debugger
-
-    # Itera no resultado e grava as marcas que ainda não estão persistidas
-    json.each do |make_params|
-      if Make.where(name: make_params["Nome"]).size == 0
-        Make.create(name: make_params["Nome"], webmotors_id: make_params["Id"])
-      end
+    # Iterates results and checks if they all exist in the DB
+    # If they not, create a new record
+    json.each do |maker_params|
+      Make.find_or_create_by(name: maker_params["Nome"], webmotors_id: maker_params["Id"])
     end
 
     @makers = Make.all
